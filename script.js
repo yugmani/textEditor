0d557299fd0085cc4576d7fccd1927d12c412e73
Himalaya <yugmani@yahoo.com>
2020-06-17 22:48:46 -0700

    oninput instead of onkeyup

--- script.js
+++ script.js
@@ -8,38 +8,40 @@ const rightEl = document.getElementById("right-align");
 const textInputEl = document.getElementById("text-input");
 const textOutputEl = document.getElementById("text-output");
 
-
+// Update the output text as a user types in the textarea
+// HINT: Use the onkeydown function inside HTML
 function updateText(){
   // CODE GOES HERE
-  
   textOutputEl.innerText = textInputEl.value;
  
 };
 
-
+// Toggle the bold class for the output text
 function makeBold(){
   //CODE GOES HERE
   textOutputEl.classList.toggle("bold");
 }
 
-
+// Toggle the italic class for the output text
 function makeItalic(){
   textOutputEl.classList.toggle("italic");
 }
 
-
+// Toggle the underline class for the output text
 function makeUnderline(elem){
     //CODE GOES HERE
-    console.log(elem);
+    // console.log(elem);
     textOutputEl.classList.toggle("underline");
 }
 
+// Toggle the style textAlign attribute
 function alignText(alignType){
   // CODE GOES HERE
   textOutputEl.style.textAlign= alignType;
   
 }
 
+//Function to change the FONT-SIZE
 function fontSizeSelect(){
   var selectEl = document.getElementById("font-size");
 
@@ -51,18 +53,18 @@ function fontSizeSelect(){
 });
 }
 
-
+//Function to change the TEXT COLOR
 function fontColorSelect(){
   var colorEl = document.getElementById("font-color");
   // colorEl.addEventListener("click", function(event){
     // event.preventDefault();
     var favColor = colorEl.value;
-    console.log(favColor);
+    // console.log(favColor);
   textOutputEl.style.color = favColor;
   // });
 }
 
-
+//Function to change FONT-FAMILY
 function fontFamilySelect(){
   var familyEl = document.getElementById("font-family");
 
